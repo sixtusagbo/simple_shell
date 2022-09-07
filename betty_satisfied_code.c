ecf7aafdbce35cb949e0389bc4820e49b49847bb
mbiman <juniormbi80@gmail.com>
2022-09-07 20:19:05 +0100

    betty compliant code

---
+++ betty_satisfied_code.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Beautiful code that passes the Betty checks
+ *
+ * Return: Always 0 - Success
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (environ[i])
+	{
+		printf("%s\n", environ[i++]);
+	}
+
+	return (0);
+}
